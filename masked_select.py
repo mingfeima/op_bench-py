8d0f53bad701d95401ab235bad96dd31fba77ddd
Mingfei Ma <mingfei.ma@intel.com>
2020-02-11 15:25:19 +0800

    masked_select validation

--- masked_select.py
+++ masked_select.py
@@ -28,8 +28,44 @@ def run_single_test(N, C):
           N, C, output.size()[0], tt))
 
 def benchmark():
-    run_single_test(128, 1000)
-    run_single_test(128, 10000)
+    #run_single_test(128, 1000)
+    #run_single_test(128, 10000)
     run_single_test(128, 100000)
 
-benchmark()
+#benchmark()
+
+def validate():
+    input = torch.randn(3, 4)
+    mask = input.ge(0.0)
+    output = torch.masked_select(input, mask)
+    print('bool mask')
+    print('input', input)
+    print('mask', mask)
+    print('output', output)
+    mask1 = mask.byte()
+    print('byte mask')
+    print('mask', mask1)
+    output1 = torch.masked_select(input, mask1)
+    print('output1', output1)
+
+#validate()
+
+def broadcast1():
+    input = torch.randn(2, 5)
+    mask = input.ge(0.0)[0]
+    print('input', input, input.size())
+    print('mask', mask, mask.size())
+    output = torch.masked_select(input, mask)
+    print('output', output)
+
+def broadcast2():
+    input = torch.randn(2, 5)
+    mask = input.ge(0.0)
+    input = input[0][0]
+    print('input, ', input, input.size())
+    print('mask', mask, mask.size())
+    output = torch.masked_select(input, mask)
+    print('output', output)
+
+broadcast1()
+broadcast2()
